155eabd11d588d70d9e8766faf978f6ac4d46659
Dineshb7 <bussadinesh@gmail.com>
2020-09-21 16:52:01 +0530

    heroku 3

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 
+
 const jsonServer = require('json-server');
+const path = require('path');
+const compression = require('compression');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+server.use(compression())
+// Serve only the static files form the dist directory
+server.use(express.static(__dirname + '/dist/Dinesh-app1'));
 
+server.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/Dinesh-app1/index.html'));
+});
 server.use(middlewares);
 server.use(router);
 
