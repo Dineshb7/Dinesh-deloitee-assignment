18210fedda7c6a6edd1671277e3acaee3bfd9a17
Dineshb7 <bussadinesh@gmail.com>
2020-09-21 16:42:04 +0530

    heroku2

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 
-const express = require('express');
-const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const compression = require('compression')
-const app = express();
-  app.use(compression())
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Dinesh-app1'));
+server.use(middlewares);
+server.use(router);
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/Dinesh-app1/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+server.listen(port);
