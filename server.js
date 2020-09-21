f4887c37ba48f5b107276d8489692063d8a8f9f8
Dineshb7 <bussadinesh@gmail.com>
2020-09-21 16:12:12 +0530

    heroku deployment

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const path = require('path');
 
+const compression = require('compression')
 const app = express();
-
+  app.use(compression())
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/Dinesh-app1'));
 
